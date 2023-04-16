a5e9872480f6ae5eaeb7e9e7cbb96ca2218ecebc
Rahul Kumar <rahul850kr@gmail.com>
2023-04-17 01:19:00 +0530

    added react app

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from "./logo.svg";
+import "./App.css";
+import { Button } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <div className="App">
+      Welcome To Chat App
+      <Button>Submit</Button>
+    </div>
+  );
+}
+
+export default App;
